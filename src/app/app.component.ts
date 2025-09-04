e5dff1112da0cb2001ede2f7b084bbfba1e9a92b
snaini45 <satwikreddy945@gmail.com>
2025-09-03 21:38:54 -0500

    fix: remove TodosComponent and update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+import { FileListComponent } from './file-list/file-list.component'; 
 
-
+// Configure Amplify once globally
 Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
+  styleUrls: ['./app.component.css'], 
+  imports: [RouterOutlet, FileListComponent, AmplifyAuthenticatorModule], 
 })
 export class AppComponent {
   title = 'amplify-angular-template';
-    
-  constructor(public authenticator: AuthenticatorService) {
-    Amplify.configure(outputs);
-  }
+
+  constructor(public authenticator: AuthenticatorService) {}
 }
