716134719b0443a974ad91f512da25207add679d
Naini, Satwik <snaini+wmemu@wm.com>
2025-09-09 09:52:04 -0500

    Added_new_json_under_asset

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-
 import { Amplify } from 'aws-amplify';
-
-// JSON imports are supported via TypeScript configuration (resolveJsonModule).
-
-import outputs from '../amplify_outputs.json'; 
+import outputs from './assets/amplify_outputs.json';
 
 Amplify.configure(outputs);
 
