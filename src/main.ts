8d68f1d8c312eb68fb7d8557e31c4042b5e08e76
Naini, Satwik <snaini+wmemu@wm.com>
2025-09-09 09:44:50 -0500

    Amplify_output.json

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json'; // adjust path if needed
+
+// JSON imports are supported via TypeScript configuration (resolveJsonModule).
+
+import outputs from '../amplify_outputs.json'; 
 
 Amplify.configure(outputs);
 
